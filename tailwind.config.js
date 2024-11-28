4756102400e5d6c89763c9847c3655d9e83bece3
hyunjeong <hyunjeong9592@gmail.com>
2024-11-28 12:44:46 +0900

    :wrench: tailwindcss 추가

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',  // Just-in-Time 모드 (JIT 모드 활성화)
+  content: ["*.{html,js}"], // Tailwind가 스타일을 적용할 파일 경로
+  theme: {
+    extend: {// 기본 테마를 확장하거나 덮어쓰기 위해 사용하는 섹션
+      fontFamily: {
+        'noto-sans': ['Noto Sans', 'sans-serif'],
+      },
+      colors: {
+        'black' : '#131313',
+        'ivory' : '#F0F0F0',
+        'gray' : '#F8FAFC',
+        'light-gray' : '#E8E9E9',
+        'dark-gray' : '#3D3E42',
+        'cream' : '#E6E5E6',
+        'light-green' : '#66CC99',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      textColor: ['hover'],
+    },  // 다양한 상태에 따른 스타일 확장 (hover, active 등)
+
+  },
+  plugins: ['@tailwindcss/forms',
+  ] // 플러그인 추가하는 곳 (예: Tailwind Forms 등)
+}

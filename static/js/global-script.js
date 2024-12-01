d69af19eb9bfeb356019390f0a3e26ce6676ee9f
hyunjeong <hyunjeong9592@gmail.com>
2024-12-01 19:39:11 +0900

    :sparkles: 이미지 클릭시 모달 팝업창실행

--- static/js/global-script.js
+++ static/js/global-script.js
@@ -17,4 +17,42 @@ document.addEventListener('DOMContentLoaded', () => {
             scrollTop: $(targetSection).offset().top - headerHeight
         }, 100, 'linear'); // 500ms 동안 스크롤 (원하는 대로 조정 가능)
     });
+
+
+
+    $(function(){
+        // 	이미지 클릭시 해당 이미지 모달
+        $(".imgC").click(function(){
+            console.log("이미지 클릭");
+            $(".modal").show();
+            // 해당 이미지 가겨오기
+            var imgSrc = $(this).children("img").attr("src");
+            var imgAlt = $(this).children("img").attr("alt");
+            $(".modalBox img").attr("src", imgSrc);
+            $(".modalBox img").attr("alt", imgAlt);
+
+            // 해당 이미지 텍스트 가져오기
+            var imgTit =  $(this).children("p").text();
+            $(".modalBox p").text(imgTit);
+
+        });
+
+        //.modal안에 button을 클릭하면 .modal닫기
+        $(".modal button").click(function(){
+            $(".modal").hide();
+        });
+
+        //.modal밖에 클릭시 닫힘
+        $(".modal").click(function (e) {
+            if (e.target.className != "modal") {
+                return false;
+            } else {
+                $(".modal").hide();
+            }
+        });
+    });
+
 });
+
+
+
